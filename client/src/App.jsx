f52ea685c9089fb815f35b7023241153142d80b2
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-06-30 00:43:27 +0600

    feat: Add loading state handling in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,23 +6,30 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { AuthContext } from "./context/AuthContext";
+import Loading from "./components/Loading";
 
 function App() {
   const [count, setCount] = useState(0);
   const location = useLocation();
   const loginPage = location.pathname.includes("/login");
   const registerPage = location.pathname.includes("/register");
-  const { user, logout } = useContext(AuthContext);
+  const { user, logout, loading } = useContext(AuthContext);
 
   return (
-    <div className="flex flex-col min-h-screen w-full">
-      {loginPage | registerPage ? "" : <Navbar />}
-      <div className="flex-1  my-5 ">
-        <MainRoutes />
-      </div>
-      {loginPage | registerPage ? "" : <Footer />}
-      <Toaster />
-    </div>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="flex flex-col min-h-screen w-full">
+          {loginPage | registerPage ? "" : <Navbar />}
+          <div className="flex-1  my-5 ">
+            <MainRoutes />
+          </div>
+          {loginPage | registerPage ? "" : <Footer />}
+          <Toaster />
+        </div>
+      )}
+    </>
   );
 }
 
