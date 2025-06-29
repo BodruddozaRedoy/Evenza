72d63a91490c342e012cac679fbf0d267c6bb84d
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-06-30 00:41:23 +0600

    feat: Update Loading component usage in PrivateRoute for loading state handling

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className='w-full h-full flex items-center justify-center'><img src="/loading.gif" alt="" /></div>
+    <div className='w-screen h-screen flex items-center justify-center'><img src="/loading.gif" alt="" /></div>
   )
 }
--- client/src/routes/PrivateRoute.jsx
+++ client/src/routes/PrivateRoute.jsx
@@ -1,10 +1,15 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../components/Loading";
 
 export default function PrivateRoute({ children }) {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
+
+  if (loading) {
+    return <Loading/>
+  }
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
