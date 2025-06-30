972bd9a11c686c76244598487781247db55d66ac
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-06-30 11:46:46 +0600

    fix: Add margin to user name in Navbar for improved spacing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -57,7 +57,7 @@ export default function Navbar() {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <li className="px-2 font-semibold">{user?.name}</li>
+                <li className="px-2 font-semibold mb-3">{user?.name}</li>
                 <li>
                   <button
                     onClick={handleLogOut}
